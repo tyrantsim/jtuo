dadf3b7582f9d07941106d45f50451a4f5ce9d85
Daniel Merchasin <archnovaelite@gmail.com>
2017-08-26 01:03:42 +0300

    DeckParser.expandDeckToList() implementation

--- src/com/github/tyrantsim/jtuo/parsers/DeckParser.java
+++ src/com/github/tyrantsim/jtuo/parsers/DeckParser.java
@@ -1,8 +1,10 @@
 package com.github.tyrantsim.jtuo.parsers;
 
+import com.github.tyrantsim.jtuo.decks.Deck;
 import com.github.tyrantsim.jtuo.util.Pair;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class DeckParser {
 
@@ -11,9 +13,10 @@ public class DeckParser {
      *          The key is the deck name, the value is the factor (weight of deck out of all decks)
      */
 
-    /* Fields from read.cpp */
-
     private static Set<String> expandingDecks = new HashSet<String>();
+
+    public static Map<String, Deck> decksByName = new HashMap<String, Deck>();
+
 
     /* Methods from read.cpp */
 
@@ -36,8 +39,57 @@ public class DeckParser {
     }
 
     public static Map<String, Double> expandDeckToList(String deckName) {
-        // TODO
-        return null;
+
+        if (expandingDecks.contains(deckName)) {
+            // Circular reference, return empty decklist
+            System.err.println("WARNING: There is a circular referred deck: " + deckName);
+            return new HashMap<String, Double>();
+        }
+
+        String deckStr = deckName;
+        Deck deck = findDeckById(deckName);
+
+        if (deck != null) {
+
+            deckStr = deck.getDeckString();
+
+            if (deckStr.contains(";") || deckStr.contains(":") || findDeckById(deckName) != null) {
+                // Deck name refers to deck list
+                expandingDecks.add(deckName);
+                Map<String, Double> deckList = parseDeckList(deckStr);
+                expandingDecks.remove(deckName);
+                return normalize(deckList);
+            }
+        }
+
+        Map<String, Double> res = new HashMap<String, Double>();
+
+        if (deckStr.length() >= 3 && deckStr.startsWith("/") && deckStr.endsWith("/")) {
+            // Deck name is or refers to a regex
+            Pattern p = Pattern.compile(deckStr.substring(1, deckStr.length() - 1));
+
+            expandingDecks.add(deckName);
+            for (String key: decksByName.keySet()) {
+                if (p.matcher(key).find()) {
+                    Map<String, Double> deckList = expandDeckToList(key);
+                    for (String it: deckList.keySet()) {
+                        res.put(it, deckList.get(it));
+                    }
+                }
+            }
+            expandingDecks.remove(deckName);
+
+            if (res.size() == 0)
+                System.err.println("Warning: Regular expression matches nothing: " + deckStr);
+
+            return normalize(res);
+
+        } else {
+            // Deck name is a normal deck
+            res.put(deckName, 1.0d);
+            return res;
+        }
+
     }
 
     public static Map<String, Double> parseDeckList(String listString) {
@@ -83,7 +135,8 @@ public class DeckParser {
         return null;
     }
 
-
-
+    public static Deck findDeckById(String deckName) {
+        return decksByName.getOrDefault(deckName, null);
+    }
 
 }
--- src/com/github/tyrantsim/jtuo/parsers/FileParser.java
+++ src/com/github/tyrantsim/jtuo/parsers/FileParser.java
@@ -1,11 +1,36 @@
 package com.github.tyrantsim.jtuo.parsers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Map;
+import java.util.Scanner;
 
 public class FileParser {
 
-    public static void readCardAbbrs(String filename) {
-        // TODO
+    public static void readCardAbbrs(String filename) throws FileNotFoundException {
+
+        File file = new File(filename);
+
+        try (Scanner sc = new Scanner(file)) {
+
+            int numLine = 0;
+
+            while (sc.hasNext()) {
+                numLine++;
+
+                String line = sc.nextLine().trim();
+
+                if (ParserUtils.isLineIgnored(line))
+                    continue;
+
+                String[] splitLine;
+                if (line.contains(":") && !(splitLine = line.split(":"))[1].trim().isEmpty()) {
+                    // TODO
+                }
+
+            }
+        }
+
     }
 
     public static Map<Integer, Integer> readCustomCards(String filename, boolean abortOnMissing) {
