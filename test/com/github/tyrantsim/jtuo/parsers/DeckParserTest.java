a42def5af8b624451a754c623834cbad612d6437
Phantom <phantom.person.1@gmail.com>
2017-09-04 23:51:53 +0300

    DeckParser test fix

--- test/com/github/tyrantsim/jtuo/parsers/DeckParserTest.java
+++ test/com/github/tyrantsim/jtuo/parsers/DeckParserTest.java
@@ -5,16 +5,16 @@ import org.junit.Test;
 
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+
 public class DeckParserTest {
 
     @Test
     public void testParseDeck() {
         String deck = "Cyrus, Infantry-1, Infantry-2, Infantry";
         Map<String, Double> deckMap = DeckParser.expandDeckToList(deck);
-        deckMap.keySet().forEach(key -> {
-            Object value = deckMap.get(key);
-            System.out.println("Key: " + key + ", value: " + value);
-        });
+        assertEquals(1, deckMap.keySet().size());
+        assertEquals((Object) 1.0, deckMap.get("Cyrus, Infantry-1, Infantry-2, Infantry"));
     }
 
 }
