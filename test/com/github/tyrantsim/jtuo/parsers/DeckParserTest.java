6206ed9ca8bb70430c64e0512bcf6adf62b50cf6
Phantom <phantom.person.1@gmail.com>
2017-09-07 23:22:17 +0300

    GetCommanderSpec test

--- test/com/github/tyrantsim/jtuo/parsers/DeckParserTest.java
+++ test/com/github/tyrantsim/jtuo/parsers/DeckParserTest.java
@@ -1,11 +1,13 @@
 package com.github.tyrantsim.jtuo.parsers;
 
+import com.github.tyrantsim.jtuo.cards.CardType;
 import com.github.tyrantsim.jtuo.decks.Deck;
 import org.junit.Test;
 
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DeckParserTest {
 
@@ -15,6 +17,13 @@ public class DeckParserTest {
         Map<String, Double> deckMap = DeckParser.expandDeckToList(deck);
         assertEquals(1, deckMap.keySet().size());
         assertEquals((Object) 1.0, deckMap.get("Cyrus, Infantry-1, Infantry-2, Infantry"));
+    }
+
+    @Test
+    public void testGetCommanderSpec() throws Exception {
+        CardsParser.initialize();
+        int halcyonId = DeckParser.parseCardSpec("Halcyon-1").getCardId();
+        assertTrue(CardsParser.getCardCopy(halcyonId).getType() == CardType.COMMANDER);
     }
 
 }
